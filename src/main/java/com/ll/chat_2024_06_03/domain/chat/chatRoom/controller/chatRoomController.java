308dec644682c440bef23662cf59daf4628e6c9a
maegon <dlagorhs99@naver.com>
2024-06-03 16:13:17 +0900

    post room make, room list(장르별 나누거나 차트 나눌때 사용해보는 것을 고려)

--- src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/controller/chatRoomController.java
+++ src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/controller/chatRoomController.java
@@ -1,14 +1,21 @@
 package com.ll.chat_2024_06_03.domain.chat.chatRoom.controller;
 
+import com.ll.chat_2024_06_03.domain.chat.chatRoom.entity.ChatRoom;
+import com.ll.chat_2024_06_03.domain.chat.chatRoom.service.ChatRoomService;
 import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 // 이걸 활용해서 종류별 차트 구분 가능하겠는 걸?
 @Controller
 @RequestMapping("/chat/room")
+@RequiredArgsConstructor
 public class chatRoomController {
+    private final ChatRoomService chatRoomService;
     @GetMapping("{roomId}")
     @ResponseBody
     public String showRoom(
@@ -19,8 +26,21 @@ public class chatRoomController {
     }
 
     @GetMapping("/make")
-    @ResponseBody
     public String showMake() {
         return "domain/chat/chatRoom/make";
+    }
+
+    @PostMapping("/make")
+    public String postMake(
+            @RequestParam(value = "name") final String name
+    ) {
+        chatRoomService.make(name);
+        return "redirect:/chat/room/make?message=Chat Room Created";
+    }
+
+    @GetMapping("/list")
+    @ResponseBody
+    public List<ChatRoom> showList() {
+        return chatRoomService.findAll();
     }
 }
