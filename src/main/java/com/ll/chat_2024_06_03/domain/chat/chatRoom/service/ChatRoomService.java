f668a7b44d0acb1e625157983eab22e3032ce033
maegon <dlagorhs99@naver.com>
2024-06-04 16:47:25 +0900

    prod ëdata auto create

--- src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/service/ChatRoomService.java
+++ src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/service/ChatRoomService.java
@@ -1,5 +1,6 @@
 package com.ll.chat_2024_06_03.domain.chat.chatRoom.service;
 
+import com.ll.chat_2024_06_03.domain.chat.chatRoom.entity.ChatMessage;
 import com.ll.chat_2024_06_03.domain.chat.chatRoom.entity.ChatRoom;
 import com.ll.chat_2024_06_03.domain.chat.chatRoom.repository.ChatRoomRepository;
 import jakarta.transaction.Transactional;
@@ -15,11 +16,12 @@ public class ChatRoomService {
     private final ChatRoomRepository chatRoomRepository;
 
     @Transactional
-    public void make(String name) {
+    public ChatRoom make(String name) {
         ChatRoom chatRoom = ChatRoom.builder()
                 .name(name)
                 .build();
         chatRoomRepository.save(chatRoom);
+        return chatRoom;
     }
 
     public List<ChatRoom> findAll() {
