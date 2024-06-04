7cce5ad8254923613d7f53724d31e6415805b209
maegon <dlagorhs99@naver.com>
2024-06-04 14:19:29 +0900

    use jpa

--- src/main/java/com/ll/chat_2024_06_03/Chat20240603Application.java
+++ src/main/java/com/ll/chat_2024_06_03/Chat20240603Application.java
@@ -2,8 +2,10 @@ package com.ll.chat_2024_06_03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // @CreatedDate, @LastModifiedDate를 사용하기 위해 필요한 어노테이션
 public class Chat20240603Application {
 
 	public static void main(String[] args) {
